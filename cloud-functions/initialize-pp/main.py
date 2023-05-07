671be9d039255bfde4ac7b22bf67d8f614906320
Jakub Fołtyn <56119515+jakfol1217@users.noreply.github.com>
2023-05-07 18:29:14 +0200

    add cloud functions

---
+++ cloud-functions/initialize-pp/main.py
@@ -0,0 +1,76 @@
+import functions_framework
+import firebase_admin
+from firebase_admin import firestore
+import os
+import requests
+import threading
+
+COLLECTION_NAME = os.environ.get("COLLECTION_NAME", "ping-pong-translations")
+LANGUAGE_CODES = os.environ.get("LANGUAGE_CODES", ["pol_Latn", "eng_Latn", "deu_Latn", "fra_Latn", "zho_Hans", "jpn_Jpan"])
+MAX_TRANSLATION_LENGTH = os.environ.get("MAX_TRANSLATION_LENGTH", 250)
+PP_URL = os.environ.get("PP_URL", "https://web-translator-ubxvqaxota-lm.a.run.app")
+
+app = firebase_admin.initialize_app()
+db = firestore.client()
+
+class InvalidLangError(Exception):
+    pass
+
+class TextLengthError(Exception):
+    pass
+
+def check_validity(ln_tra, ln_pro, text):
+    if ln_tra not in LANGUAGE_CODES or ln_pro not in LANGUAGE_CODES:
+        raise InvalidLangError
+    if len(text) > MAX_TRANSLATION_LENGTH:
+        raise TextLengthError
+
+@firestore.transactional
+def get_status_id(transaction, document_ref):
+    document = document_ref.get(transaction=transaction)
+    status = document.get("Status")
+    id = document.id
+    return status, id
+
+def start_translating(url):
+    requests.post(url)
+
+@functions_framework.http
+def initialize_pp(request):
+    ln_tra = request.form.get("ln_tra", None)
+    ln_pro = request.form.get("ln_pro", None)
+    text = request.form.get("OriginalText", None)
+    if ln_tra is None or ln_pro is None or text is None:
+        return {"status": "empty-fail"}, 500, ""
+    col_ref = db.collection(COLLECTION_NAME)
+    exists_query = col_ref.where(
+        "OriginalText", "==", text).where(
+        "ToLanguage", "==", ln_tra).where(
+        "FromLanguage", "==", ln_pro).get()
+    if len(exists_query) > 0:
+        get_transaction = db.transaction()
+        status, id = get_status_id(get_transaction, exists_query[0].reference)
+        if status != "Pending":
+            return id
+    else:
+        try:
+            check_validity(ln_tra, ln_pro, text)
+        except InvalidLangError:
+            return {"status": "fail-lang"}, 500
+        except TextLengthError:
+            return {"status": "fail-textLength"}, 500
+
+        data = {
+            "FromLanguage": ln_pro,
+            "ToLanguage": ln_tra,
+            "OriginalText": text,
+            "CreateDate": firestore.SERVER_TIMESTAMP,
+            "Status": "Pending",
+        }
+        _, city_ref = col_ref.add(data)
+        id = city_ref.id
+    url = PP_URL + "/" + str(id)
+    thread = threading.Thread(target=start_translating, args=(url,))
+    thread.start()
+
+    return id
---
+++ cloud-functions/resume-pp/main.py
@@ -0,0 +1,58 @@
+import functions_framework
+import firebase_admin
+from firebase_admin import firestore
+import os
+import requests
+
+COLLECTION_NAME = os.environ.get("COLLECTION_NAME", "ping-pong-translations")
+LANGUAGE_CODES = os.environ.get("LANGUAGE_CODES", ["pol_Latn", "eng_Latn", "deu_Latn", "fra_Latn", "zho_Hans", "jpn_Jpan"])
+MAX_TRANSLATION_LENGTH = os.environ.get("MAX_TRANSLATION_LENGTH", 250)
+PP_URL = os.environ.get("PP_URL", "https://web-translator-ubxvqaxota-lm.a.run.app")
+
+app = firebase_admin.initialize_app()
+db = firestore.client()
+
+class PendingError(Exception):
+    pass
+
+class LoopedError(Exception):
+    pass
+
+class RunningError(Exception):
+    pass
+
+@firestore.transactional
+def lock_status(transaction, document_ref):
+    document = document_ref.get(transaction=transaction)
+    status = document.get("Status")
+    if status == "Paused":
+        transaction.update(document_ref, {"Status": "Pending", "PingPongModifyDate": firestore.SERVER_TIMESTAMP})
+    elif status == "Looped":
+        raise LoopedError
+    elif status == "Pending":
+        raise PendingError
+    else:
+        raise RunningError
+
+@functions_framework.http
+def resume_pp(request):
+    id = request.form.get("id", None)
+    if id is None:
+        return {"status": "fail-empty"}, 500
+    lock_transaction = db.transaction()
+    document_ref = db.collection(COLLECTION_NAME).document(id)
+
+    try:
+        lock_status(lock_transaction, document_ref)
+    except LoopedError:
+        return {"status": "fail-looped"}, 500
+    except PendingError:
+        return {"status": "fail-pending"}, 500
+    except RunningError:
+        return {"status": "fail-running"}, 500
+    url = PP_URL + "/" + str(id)
+    res = requests.post(url)
+    res_dict = res.json()
+    if res_dict["status"] != "success":
+        return {"status": "fail-toResume"}, 500
+    return {"status": "success"}, 200
